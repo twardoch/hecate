8ce21215282b3edc34bd0956ea426ecc2e64c092
Adam Twardoch <adam+github@twardoch.com>
2025-06-25 23:29:51 +0200

    Auto-commit: Save local changes

---
+++ .build_release/src/hecate/gflseg.d
@@ -0,0 +1,94 @@
+.build_release/src/hecate/gflseg.o: src/hecate/gflseg.cpp \
+  /usr/local/include/hecate/gflseg.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/opencv.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/opencv_modules.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/core/core_c.h \
+  /usr/local/opt/opencv@2/include/opencv2/core/types_c.h \
+  /usr/local/opt/opencv@2/include/opencv2/core/core.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/core/version.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/core/operations.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/core/mat.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/flann/miniflann.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/flann/defines.h \
+  /usr/local/opt/opencv@2/include/opencv2/flann/config.h \
+  /usr/local/opt/opencv@2/include/opencv2/imgproc/imgproc_c.h \
+  /usr/local/opt/opencv@2/include/opencv2/imgproc/types_c.h \
+  /usr/local/opt/opencv@2/include/opencv2/imgproc/imgproc.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/photo/photo.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/photo/photo_c.h \
+  /usr/local/opt/opencv@2/include/opencv2/video/video.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/video/tracking.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/video/background_segm.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/features2d/features2d.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/objdetect/objdetect.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/calib3d/calib3d.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/core/affine.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/ml/ml.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/highgui/highgui_c.h \
+  /usr/local/opt/opencv@2/include/opencv2/highgui/highgui.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/contrib/contrib.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/contrib/retina.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/contrib/openfabmap.hpp \
+  /usr/local/include/hecate/sort.hpp
+
+/usr/local/include/hecate/gflseg.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/opencv.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/opencv_modules.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/core/core_c.h:
+
+/usr/local/opt/opencv@2/include/opencv2/core/types_c.h:
+
+/usr/local/opt/opencv@2/include/opencv2/core/core.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/core/version.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/core/operations.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/core/mat.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/flann/miniflann.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/flann/defines.h:
+
+/usr/local/opt/opencv@2/include/opencv2/flann/config.h:
+
+/usr/local/opt/opencv@2/include/opencv2/imgproc/imgproc_c.h:
+
+/usr/local/opt/opencv@2/include/opencv2/imgproc/types_c.h:
+
+/usr/local/opt/opencv@2/include/opencv2/imgproc/imgproc.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/photo/photo.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/photo/photo_c.h:
+
+/usr/local/opt/opencv@2/include/opencv2/video/video.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/video/tracking.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/video/background_segm.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/features2d/features2d.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/objdetect/objdetect.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/calib3d/calib3d.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/core/affine.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/ml/ml.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/highgui/highgui_c.h:
+
+/usr/local/opt/opencv@2/include/opencv2/highgui/highgui.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/contrib/contrib.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/contrib/retina.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/contrib/openfabmap.hpp:
+
+/usr/local/include/hecate/sort.hpp:
---
+++ .build_release/src/hecate/hecate_main.d
@@ -0,0 +1,126 @@
+.build_release/src/hecate/hecate_main.o: src/hecate/hecate_main.cpp \
+  /usr/local/include/hecate/hecate.hpp \
+  /usr/local/include/hecate/sort.hpp /usr/local/include/hecate/time.hpp \
+  /usr/local/include/hecate/gapstat.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/opencv.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/opencv_modules.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/core/core_c.h \
+  /usr/local/opt/opencv@2/include/opencv2/core/types_c.h \
+  /usr/local/opt/opencv@2/include/opencv2/core/core.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/core/version.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/core/operations.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/core/mat.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/flann/miniflann.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/flann/defines.h \
+  /usr/local/opt/opencv@2/include/opencv2/flann/config.h \
+  /usr/local/opt/opencv@2/include/opencv2/imgproc/imgproc_c.h \
+  /usr/local/opt/opencv@2/include/opencv2/imgproc/types_c.h \
+  /usr/local/opt/opencv@2/include/opencv2/imgproc/imgproc.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/photo/photo.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/photo/photo_c.h \
+  /usr/local/opt/opencv@2/include/opencv2/video/video.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/video/tracking.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/video/background_segm.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/features2d/features2d.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/objdetect/objdetect.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/calib3d/calib3d.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/core/affine.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/ml/ml.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/highgui/highgui_c.h \
+  /usr/local/opt/opencv@2/include/opencv2/highgui/highgui.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/contrib/contrib.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/contrib/retina.hpp \
+  /usr/local/opt/opencv@2/include/opencv2/contrib/openfabmap.hpp \
+  /usr/local/include/hecate/knapsack.hpp \
+  /usr/local/include/hecate/cc_parser.hpp \
+  /usr/local/include/hecate/file_helper.hpp \
+  /usr/local/include/hecate/video_parser.hpp \
+  /usr/local/include/hecate/gflseg.hpp \
+  /usr/local/include/hecate/shot_range.hpp \
+  /usr/local/include/hecate/hist_opencv.hpp \
+  /usr/local/include/hecate/image_metrics.hpp \
+  /usr/local/include/hecate/ffmpeg_helper.hpp
+
+/usr/local/include/hecate/hecate.hpp:
+
+/usr/local/include/hecate/sort.hpp:
+
+/usr/local/include/hecate/time.hpp:
+
+/usr/local/include/hecate/gapstat.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/opencv.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/opencv_modules.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/core/core_c.h:
+
+/usr/local/opt/opencv@2/include/opencv2/core/types_c.h:
+
+/usr/local/opt/opencv@2/include/opencv2/core/core.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/core/version.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/core/operations.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/core/mat.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/flann/miniflann.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/flann/defines.h:
+
+/usr/local/opt/opencv@2/include/opencv2/flann/config.h:
+
+/usr/local/opt/opencv@2/include/opencv2/imgproc/imgproc_c.h:
+
+/usr/local/opt/opencv@2/include/opencv2/imgproc/types_c.h:
+
+/usr/local/opt/opencv@2/include/opencv2/imgproc/imgproc.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/photo/photo.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/photo/photo_c.h:
+
+/usr/local/opt/opencv@2/include/opencv2/video/video.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/video/tracking.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/video/background_segm.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/features2d/features2d.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/objdetect/objdetect.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/calib3d/calib3d.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/core/affine.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/ml/ml.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/highgui/highgui_c.h:
+
+/usr/local/opt/opencv@2/include/opencv2/highgui/highgui.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/contrib/contrib.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/contrib/retina.hpp:
+
+/usr/local/opt/opencv@2/include/opencv2/contrib/openfabmap.hpp:
+
+/usr/local/include/hecate/knapsack.hpp:
+
+/usr/local/include/hecate/cc_parser.hpp:
+
+/usr/local/include/hecate/file_helper.hpp:
+
+/usr/local/include/hecate/video_parser.hpp:
+
+/usr/local/include/hecate/gflseg.hpp:
+
+/usr/local/include/hecate/shot_range.hpp:
+
+/usr/local/include/hecate/hist_opencv.hpp:
+
+/usr/local/include/hecate/image_metrics.hpp:
+
+/usr/local/include/hecate/ffmpeg_helper.hpp:
